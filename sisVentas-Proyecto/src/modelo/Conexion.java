106457bb327eea63f64684e5b9732a95176a26fa
Ferrero Marcos <85361925+MarcoS211@users.noreply.github.com>
2023-10-02 12:51:19 -0300

    First Commit

---
+++ sisVentas-Proyecto/src/modelo/Conexion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLClientInfoException;
+
+public class Conexion {
+    private Connection conexion;
+    
+    public Conexion(){
+    }
+}
